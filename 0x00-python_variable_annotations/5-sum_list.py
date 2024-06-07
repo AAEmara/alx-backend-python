e45aca93e20fbab4b5169c6ea4c94e173eb34b32
AAEmara <abdelrahmanemara18@gmail.com>
2024-06-08 00:59:11 +0300

    0x00: Task5 Python Script.
    Using type annotations to return a float
    (the sum of items in the given list) for
    the argument which is type-annotated with
    a list of floats.

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""A module that defines sum_list() function using type-annotations."""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Returns the sum of numbers in a list.
+
+    Args:
+        input_list: The list that contains float items.
+
+    Returns:
+        The sum of the items inside of the given list.
+    """
+    total: float = 0.0
+
+    for num in input_list:
+        total += num
+
+    return (total)
