965deef7cadaa00cde25fb29142fa49c05ac433a
AAEmara <abdelrahmanemara18@gmail.com>
2024-06-08 00:22:52 +0300

    0x00: Task3 Python Script.
    Using the type-annotated function to take
    a float argument and return a string output.

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""A module that creates a to_str() function with type-annotation."""
+
+
+def to_str(n: float) -> str:
+    """Returns a string representation of the argument.
+
+    Args:
+        n: A float argument.
+
+    Returns:
+        The string representation of the float argument.
+    """
+    return (str(n) + ',')
