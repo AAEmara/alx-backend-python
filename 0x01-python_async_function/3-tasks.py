396bd8b1f668636e1c443cfd3aa40befa672c443
AAEmara <abdelrahmanemara18@gmail.com>
2024-06-11 05:28:52 +0300

    0x01: Task3 Python Script - Requirments Fix 2.
    Fix the type hint of the output to be
    asyncio.Task.

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -7,7 +7,7 @@ import typing
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-def task_wait_random(max_delay: int) -> typing.Awaitable:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """Creates a task with asyncio.
     Args:
         max_delay: The upper bound of delaying seconds.
