ba7037e6bc4867ba6755ff0af767ea27c3472c98
AAEmara <abdelrahmanemara18@gmail.com>
2024-06-11 03:10:07 +0300

    0x01: Task0 Python Script - asyncio & random.
    Used asyncio and random library functions.

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""A module that defines an asynchronous coroutine."""
+
+import asyncio
+import random
+import typing
+
+
+async def wait_random(max_delay: typing.Union[int, float] = 10)\
+        -> typing.Union[int, float]:
+    """Asynchronous coroutine that wait for random delay and
+    returns its value.
+
+    Args:
+        max_delay: A random number of seconds to wait for.
+
+    Returns:
+        The max_delay argument.
+    """
+    sleep_time: float = random.uniform(0, max_delay)
+    await asyncio.sleep(sleep_time)
+
+    return (sleep_time)
