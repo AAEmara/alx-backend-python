ac5299111ff6b89d11953e3664a0480a0f06c2b5
AAEmara <abdelrahmanemara18@gmail.com>
2024-06-11 05:14:04 +0300

    0x01: Task3 Python Script - Requirements Fix.
    Added the output type-annotation of asyncio.Task
    to be asyncio.Awaitable.

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,11 +2,12 @@
 """A module that defines a function that returns asyncio.Task."""
 
 import asyncio
+import typing
 
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-def task_wait_random(max_delay: int):
+def task_wait_random(max_delay: int) -> typing.Awaitable:
     """Creates a task with asyncio.
     Args:
         max_delay: The upper bound of delaying seconds.
