a1169a5526c71e0d518ab887d832b365d7f13d6a
AAEmara <abdelrahmanemara18@gmail.com>
2024-06-11 15:22:01 +0300

    0x02: Task1 Python Script - Fix Output.
    Fixing the typing-annotation for the async
    function output.

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,7 +7,7 @@ from typing import Generator, List
 async_generator = __import__("0-async_generator").async_generator
 
 
-async def async_comprehension() -> Generator[List[float], None, None]:
+async def async_comprehension() -> Generator[float, None, None]:
     """Uses a coroutine to return 10 random numbers from it.
 
     Args:
