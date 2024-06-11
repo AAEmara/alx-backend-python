cd62bef510d7fa84ea956dde1373e72eef78d33e
AAEmara <abdelrahmanemara18@gmail.com>
2024-06-11 14:51:22 +0300

    0x02: Task0 Python Script - Async Generator.
    Used an Async Generator to be applied later on
    in an Async Comprehension.

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""A module that defines a coroutine."""
+
+import asyncio
+import random
+import typing
+
+
+async def async_generator() -> float:
+    """Generates a list of random numbers asynchronously.
+
+    Args:
+        NOTHING
+
+    Returns:
+        A list of random numbers.
+    """
+    for i in range(10):
+        yield random.uniform(0, 10)
+        await asyncio.sleep(1)
