0936d92db5e1c66bd948451da16c04201b25ffe3
AAEmara <abdelrahmanemara18@gmail.com>
2024-06-11 15:19:53 +0300

    0x02: Task1 Python Script - Async Comprehensions.
    Applied Async Comprehensions on the asynchronous
    function.

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""A module that defines a coroutine."""
+
+import asyncio
+from typing import Generator, List
+
+async_generator = __import__("0-async_generator").async_generator
+
+
+async def async_comprehension() -> Generator[List[float], None, None]:
+    """Uses a coroutine to return 10 random numbers from it.
+
+    Args:
+        NOTHING
+
+    Returns:
+        A list of 10 random numbers.
+    """
+    results = [i async for i in async_generator()]
+    return (results)
