159472b73901f22ac39ed56f6dfb9c36cdc75ce5
AAEmara <abdelrahmanemara18@gmail.com>
2024-07-05 19:10:02 +0300

    0x03: Task3 Python Script - Mocking a Decorator.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 
 import unittest
 from unittest.mock import patch, Mock
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
 
 
@@ -55,3 +55,38 @@ class TestGetJson(unittest.TestCase):
             result = get_json(url)
             mock_get.assert_called_once_with(url)
             self.assertEqual(result, payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """A class that includes tests for the `memoize` function.
+    """
+    def test_memoize(self):
+        """Test a method that is already cached `memoized`.
+        """
+
+        class TestClass:
+            """A Class that is going to be tested for memoization.
+            """
+            def a_method(self):
+                """A method that returns a value.
+                Args:
+                    NOTHING
+                Returns:
+                    Return an integer of a value equals to 42.
+                """
+                return 42
+
+            @memoize
+            def a_property(self):
+                """A method that is going to be memoized.
+                """
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method") as mock_a_method:
+            mock_a_method.return_value = 42
+            test_instance = TestClass()
+            result1 = test_instance.a_property
+            result2 = test_instance.a_property
+            mock_a_method.assert_called_once_with()
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
