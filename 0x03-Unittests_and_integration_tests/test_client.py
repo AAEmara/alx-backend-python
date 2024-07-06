7f4dbb621e75a42fe7f4082c3ae4aa361f56032b
AAEmara <abdelrahmanemara18@gmail.com>
2024-07-06 18:50:16 +0300

    0x03: Task4 Python Script - Reverting to My Code.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python3
-"""Testing functions of the client module
-using patchers as decorator and as context manager
-- This file has unit-tests and integration tests.
-"""
+"""A module that include test cases for `client` module."""
 
 
-import unittest
 from client import GithubOrgClient
-from parameterized import parameterized, parameterized_class
-from unittest.mock import patch, PropertyMock
-from fixtures import TEST_PAYLOAD
+from parameterized import parameterized
+from typing import Dict
+import unittest
+from unittest.mock import patch, MagicMock
 
 
 class TestGithubOrgClient(unittest.TestCase):
+    """A Class that includes tests for `GithubOrgClient` class.
     """
-    Class for testing: client.GithubOrgClient.org
-    - This class has unit-tests.
-    """
+
     @parameterized.expand([
-        ("google",),
-        ("abc",)
+        ["google", {"payload": True}],
+        ["abc", {"payload": False}]
     ])
-    @patch('client.get_json')
-    def test_org(self, org_name, mock_get_json):
-        """Test GithubOrgClient.org function."""
-        org_client = GithubOrgClient(org_name)
-        url = f'https://api.github.com/orgs/{org_name}'
-        mock_get_json.return_value = {"name": "abd"}
-        self.assertEqual(org_client.org, {"name": "abd"})
-        mock_get_json.assert_called_once_with(url)
+    @patch("client.get_json")
+    def test_org(self, org: str, result: Dict, mock_get: MagicMock) -> None:
+        """Testing the `GithubOrgClient.org` returned value.
+        Args:
+            org_name: The name of the organization used in GitHub.
+            expected_result: The expected payload returned result.
+            mock_get: The mocked function used in the method.
+        """
+        test_instance = GithubOrgClient(org)
+        url: str = f"https://api.github.com/orgs/{org}"
+        mock_get.return_value: Dict = result
+        test_result: Dict = test_instance.org
+        mock_get.assert_called_once_with(url)
+        self.assertEqual(test_result, result)
