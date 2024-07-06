ff28e394a10ca1a953759b9f3226dbe75c9dbcf5
AAEmara <abdelrahmanemara18@gmail.com>
2024-07-06 18:38:57 +0300

    0x03: Task4 Python Script - MockMagic Duck Typing.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -6,7 +6,7 @@ from client import GithubOrgClient
 from parameterized import parameterized
 from typing import Dict
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import patch, MagicMock
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -18,16 +18,16 @@ class TestGithubOrgClient(unittest.TestCase):
         ["abc", {"payload": False}]
     ])
     @patch("client.get_json")
-    def test_org(self, org: str, result: Dict, mock_get_json: Mock) -> None:
+    def test_org(self, org: str, result: Dict, mock_get: MagicMock) -> None:
         """Testing the `GithubOrgClient.org` returned value.
         Args:
             org_name: The name of the organization used in GitHub.
             expected_result: The expected payload returned result.
-            mock_get_json: The mocked function used in the method.
+            mock_get: The mocked function used in the method.
         """
         test_instance = GithubOrgClient(org)
         url: str = f"https://api.github.com/orgs/{org}"
-        mock_get_json.return_value: Dict = result
+        mock_get.return_value: Dict = result
         test_result: Dict = test_instance.org
-        mock_get_json.assert_called_once_with(url)
+        mock_get.assert_called_once_with(url)
         self.assertEqual(test_result, result)
