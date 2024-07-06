63f31fe0f06db78e07b2c21649e9d2470a30ad93
AAEmara <abdelrahmanemara18@gmail.com>
2024-07-06 18:08:08 +0300

    0x03: Task4 Python Script - Fix Ouput 4.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -18,15 +18,16 @@ class TestGithubOrgClient(unittest.TestCase):
         ["abc", "https://api.github.com/orgs/abc"]
     ])
     @patch("client.get_json")
-    def test_org(self, org: str, url: str, mock_get_json: Mock):
+    def test_org(self, org: str, url: str, mock_get_json: Mock) -> None:
         """Testing the `GithubOrgClient.org` returned value.
         Args:
-            org_name: The name of the organization used in GitHub.
-            expected_result: The expected payload returned result.
+            org: The name of the organization used in GitHub.
+            url: The expected payload returned result.
             mock_get_json: The mocked function used in the method.
         """
         instance = GithubOrgClient(org)
         result1 = instance.org
         result2 = instance.org
+        result3 = instance.org
 
         mock_get_json.assert_called_once_with(url)
