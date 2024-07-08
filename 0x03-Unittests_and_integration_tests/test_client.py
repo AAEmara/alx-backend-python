2a32066eae05a64884563afe49076e8dab95182a
AAEmara <abdelrahmanemara18@gmail.com>
2024-07-08 04:46:02 +0300

    0x03: Task6 Python Script - Patching.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -44,3 +44,19 @@ class TestGithubOrgClient(unittest.TestCase):
             tested_repo_url = org_instance._public_repos_url  # Returned URL
             # Testing the returned url vs the expected url.
             self.assertEqual(tested_repo_url, expected_repo_url)
+
+    @patch("client.get_json")
+    def test_public_repos(self, mock_get_json: MagicMock) -> None:
+        """Testing the `GithubOrgClient.public_repos` returned value.
+        """
+        method: str = "_public_repos_url"
+        payload: Dict = [{"name": "repo1"}, {"name": "repo2"}]
+        url: str = "https://api.github.com/google"
+        with patch.object(GithubOrgClient, method) as mock_public_repos_url:
+            mock_public_repos_url.return_value = url
+            mock_get_json.return_value = payload
+            org_instance = GithubOrgClient("google")
+            returned_url = org_instance._public_repos_url
+            tested_repos = org_instance.public_repos(None)
+            expected_repos = ["repo1", "repo2"]
+            self.assertEqual(tested_repos, expected_repos)
